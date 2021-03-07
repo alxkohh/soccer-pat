d8813ffe48682db9d0dbb7921f8651241fa98d76
Alex <alxkohh@gmail.com>
2021-03-08 01:38:11 +0800

    Add new football library

---
+++ csharp/PAT.Lib.Football.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// namespace must be PAT.Lib
+namespace PAT.Lib {
+	
+	/*
+		Syntax to invoke ball_move_forward(...) in PAT:
+		
+		call(ball_move_forward, teamA, 2, ball_loc)
+		
+		call is a keyword in PAT
+		
+		Also, all methods need to be public static
+	*/
+	
+    public class Football {
+	     
+	 	private static int teamA = 1;
+	    
+	    // ball_move_forward(teamA, 2, ball_loc) means the ball move forward 2 zones for teamA. new ball location is returned
+		public static int ball_move_forward(int team, int numZones, int loc) {
+			
+			if (team == teamA) {
+				return ball_move_forward_teamA(numZones, loc);
+			} else {	// team == teamB
+				return ball_move_forward_teamB(numZones, loc);
+			}
+		}
+	    
+	    public static int ball_move_forward_teamA(int numZones, int loc) {
+			
+			if (numZones == 1) {
+				
+				int new_loc = loc + 20;
+				
+				// the furthest teamA can go is 80
+				if (new_loc > 80) {
+					return 80;
+				} else {
+					return new_loc;
+				}
+			} else {	// numZones == 2
+				
+				int new_loc = loc + 40;
+				
+				// the furthest teamA can go is 80
+				if (new_loc > 80) {
+					return 80;
+				} else {
+					return new_loc;
+				}
+			}
+		}
+		
+		public static int ball_move_forward_teamB(int numZones, int loc) {
+			
+			if (numZones == 1) {
+				
+				int new_loc = loc - 20;
+				
+				// the furthest teamB can go is 20
+				if (new_loc < 20) {
+					return 20;
+				} else {
+					return new_loc;
+				}
+			} else {	// numZones == 2
+				
+				int new_loc = loc - 40;
+				
+				// the furthest teamB can go is 20
+				if (new_loc < 20) {
+					return 20;
+				} else {
+					return new_loc;
+				}
+			}
+		} 
+	     
+    }
+}
