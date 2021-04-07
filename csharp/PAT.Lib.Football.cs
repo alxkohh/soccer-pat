1a7014e9b2feed3359fc444b31d0b655efd88e5c
Alfred <alyip98@gmail.com>
2021-04-07 18:59:18 +0800

    Update PAT to use scores

--- csharp/PAT.Lib.Football.cs
+++ csharp/PAT.Lib.Football.cs
@@ -1,68 +1,114 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 // namespace must be PAT.Lib
+
+using System;
+
 namespace PAT.Lib {
 	
-	/*
-		Syntax to invoke ball_move_forward(...) in PAT:
-		
-		call(ball_move_forward, teamA, 2, ball_loc)
-		
-		call is a keyword in PAT
-		
-		Also, all methods need to be public static
-	*/
+    /*
+        Syntax to invoke ball_move_forward(...) in PAT:
+        
+        call(ball_move_forward, teamA, 2, ball_loc)
+        
+        call is a keyword in PAT
+        
+        Also, all methods need to be public static
+    */
 	
     public class Football {
 	     
-	 	private static int teamA = 0;
-	 	private static int zones = 10;
-	 	
-	 	public static void setZones(int _zones) {
-	 		zones = _zones;
-	 	}
-	 	
-	 	public static int opponent_of(int team) {
-	 		return 1 - team;
-	 	}
-	 	
-	 	public static int success_rate(int team, int ball_loc) {
-			if (team == teamA) {
-				return 100 - ball_loc * 100 / zones;
-			} else {
-				return ball_loc * 100 / zones;
-			}
-		}
+        private static int teamA = 0;
+        private static int zones = 4;
+        private static int shortPassDistance = 1;
+        private static int longPassDistance = 2;
+        
+        private static double shoot_k = 3.049;
+        private static double shoot_x0 = 1.766;
+        private static double s_pass_k = 2.024;
+        private static double s_pass_x0 = 0.033;
+        private static double l_pass_k = 3.047;
+        private static double l_pass_x0 = 0.888;
+        private static double dribble_k = 1.417;
+        private static double dribble_x0 = 0.730;
+
+        public static void Config(int _zones, int _shortPassDistance, int _longPassDistance)
+        {
+            zones = _zones;
+            shortPassDistance = _shortPassDistance;
+            longPassDistance = _longPassDistance;
+        }
+        
+        public static void setZones(int _zones) {
+            zones = _zones;
+        }
+
+        public static int opponent_of(int team) {
+            return 1 - team;
+        }
+
+        private static double L(double x, double k, double x0)
+        {
+            return 100 / (1 + Math.Exp(-k * (x - x0)));
+        }
+
+        public static int shoot_success_rate(int team, int ball_loc, int scoreA, int scoreB)
+        {
+            var ratio = (double) scoreA / scoreB;
+            return (int) L(ratio, shoot_k, shoot_x0);
+        }
+        
+        public static int short_pass_success_rate(int team, int ball_loc, int scoreA, int scoreB)
+        {
+            var ratio = (double) scoreA / scoreB;
+            return (int) L(ratio, s_pass_k, s_pass_x0);
+        }
+        
+        public static int long_pass_success_rate(int team, int ball_loc, int scoreA, int scoreB)
+        {
+            var ratio = (double) scoreA / scoreB;
+            return (int) L(ratio, l_pass_k, l_pass_x0);
+        }
+        
+        public static int dribble_success_rate(int team, int ball_loc, int scoreA, int scoreB)
+        {
+            var ratio = (double) scoreA / scoreB;
+            return (int) L(ratio, dribble_k, dribble_x0);
+        }
+
+        public static int success_rate(int team, int ball_loc) {
+            if (team == teamA) {
+                return (zones - ball_loc) * 100/zones;
+            } else {
+                return ball_loc * 100/zones;
+            }
+        }
 	    
-	    // ball_move_forward(teamA, 2, ball_loc) means the ball move forward 2 zones for teamA. new ball location is returned
-		public static int ball_move_forward(int team, int numZones, int loc) {
-			
-			if (team == teamA) {
-				return ball_move_forward_teamA(numZones, loc);
-			} else {	// team == teamB
-				return ball_move_forward_teamB(numZones, loc);
-			}
-		}
-	    
-	    public static int ball_move_forward_teamA(int numZones, int loc) {
-			int new_loc = loc + numZones;
-			if (new_loc > zones-2) {
-				return zones-2;
-			} else {
-				return new_loc;
-			}
-		}
-		
-		public static int ball_move_forward_teamB(int numZones, int loc) {
-			int new_loc = loc - numZones;
-			if (new_loc < 2) {
-				return 2;
-			} else {
-				return new_loc;
-			}
-		} 
+        // ball_move_forward(teamA, 2, ball_loc) means the ball move forward 2 zones for teamA. new ball location is returned
+        public static int ball_move_forward(int team, int numZones, int loc) {
+            if (team == teamA) {
+                return ball_move_forward_teamA(numZones, loc);
+            } else {	// team == teamB
+                return ball_move_forward_teamB(numZones, loc);
+            }
+        }
+        private static int ball_move_forward_teamA(int numZones, int loc) {
+            int newLoc = loc + numZones;
+            // the furthest teamA can go is 80
+            if (newLoc > zones - 1) {
+                return zones - 1;
+            } else {
+                return newLoc;
+            }
+        }
+
+        private static int ball_move_forward_teamB(int numZones, int loc) {
+			int newLoc = loc + numZones;
+            // the furthest teamB can go is 1
+            if (newLoc < 1) {
+                return 1;
+            } else {
+                return newLoc;
+            }
+        } 
 	     
     }
 }
